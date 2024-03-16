17a7d68ab33017f550a9f19211b279bfccc2eab9
M.Arya Suherman <123355017+MuhammadAryaSuherman@users.noreply.github.com>
2024-03-16 15:22:54 +0700

    membuat fitur pelanggan

--- app/controller/Pelanggan.php
+++ app/controller/Pelanggan.php
@@ -1,5 +1,8 @@
 <?php
+<<<<<<< Updated upstream
 require_once '../app/config/config.php';
+=======
+>>>>>>> Stashed changes
 class Pelanggan extends Controller {
     private $model;
 
@@ -8,6 +11,7 @@ class Pelanggan extends Controller {
     }
 
     public function index() {
+<<<<<<< Updated upstream
         $data['judul'] = "Pelanggan";
         $data['pelanggan'] = $this->model->getAllPelanggan();
         $this->view('templates/header', $data);
@@ -25,16 +29,34 @@ class Pelanggan extends Controller {
 
     public function tambah() {
         if( $this->model->tambahPelanggan($_POST) > 0 ) {
+=======
+        $data["judul"] = "Pelanggan";
+        $data['pelanggan'] = $this->model("PelangganModel")->getAllPelanggan();
+        $this->view("templates/header", $data);
+        $this->view("pelanggan/index", $data);
+    }
+
+    public function tambah() {
+        if($this->model('PelangganModel')->tambahpelanggan($_POST) > 0) {
+>>>>>>> Stashed changes
             Flasher::setFlash('berhasil', 'ditambahkan', 'success');
             header('Location: ' . BASE_URL . '/pelanggan');
             exit;
-        } else {
-            Flasher::setFlash('gagal', 'ditambahkan', 'danger');
+         } else {
+            Flasher::setFlash('gagal', 'ditambahkan', 'danger');   
             header('Location: ' . BASE_URL . '/pelanggan');
             exit;
-        }
+          }
+     }
+
+     public function delete($id) {
+        $pelangganModel = $this->model('PelangganModel');
+        $pelangganModel->deletePelanggan($id);
+        Flasher::setFlash('berhasil', 'didelete', 'success');
+        header('Location: ' . BASE_URL . '/pelanggan');
     }
 
+<<<<<<< Updated upstream
     public function ubah() {
         if( $this->model->updatePelanggan($_POST) > 0 ) {
             Flasher::setFlash('berhasil', 'diubah', 'success');
@@ -56,7 +78,24 @@ class Pelanggan extends Controller {
             Flasher::setFlash('gagal', 'dihapus', 'danger');
             header('Location: ' . BASE_URL . '/pelanggan');
             exit;
+=======
+    public function edit() {
+        $pelangganModel = $this->model('PelangganModel');
+        $data = [
+            'id_pelanggan' => $_POST['id_pelanggan'],
+            'nama_pelanggan' => $_POST['nama_pelanggan'],
+            'alamat' => $_POST['alamat'],
+            'telepon' => $_POST['telepon']
+        ];
+        if ($pelangganModel->editPelanggan($data)) {
+            Flasher::setFlash('berhasil', 'diupdate', 'success');
+        } else {
+            Flasher::setFlash('gagal', 'diupdate', 'danger');
+>>>>>>> Stashed changes
         }
+        header('Location: ' . BASE_URL . '/pelanggan');
     }
+    
+    
 }
 ?>
